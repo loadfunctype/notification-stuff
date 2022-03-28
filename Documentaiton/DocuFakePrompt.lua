27a50234595abfeaba5fa75b677e0971c54722d9
x <66913721+jaysswag@users.noreply.github.com>
2022-03-28 11:27:31 -0300

    fully functional

--- Documentaiton/DocuFakePrompt.lua
+++ Documentaiton/DocuFakePrompt.lua
@@ -1,4 +1,4 @@
-local prompt = loadstring(game:HttpGet('https://www.floppa.dev/FakePrompt.lua', true))()
+local prompt = loadstring(game:HttpGet('https://raw.githubusercontent.com/jaysswag/notification-stuff/main/Modules/FakePrompt.lua', true))()
 prompt.createPrompt("title", "text", "button text", true, function(close) 
       print("Button Clicked") -- script here, do whatever you want.
       close() -- prompt.createPrompt(title text, message text, button text, blurred, button click callback)  
