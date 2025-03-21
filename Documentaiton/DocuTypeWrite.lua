9e06ac64a647e8d8472e4d446b58078d04e25dc5
loadfunctype <66913721+loadfunctype@users.noreply.github.com>
2025-03-21 01:11:29 -0300

    Create DocuTypeWrite.lua

---
+++ Documentaiton/DocuTypeWrite.lua
@@ -0,0 +1,8 @@
+local TypewriterNotification = loadstring(game:HttpGet("https://github.com/AlexR32/Roblox/raw/refs/heads/main/Useful/TypeWriter.lua"))()
+
+-- Can stack like a list
+TypewriterNotification("super dupe nig", 5, 0.05)
+task.wait(2)
+TypewriterNotification("super dupe nig x2", 5, 0.05)
+task.wait(2)
+TypewriterNotification("super dupe nig x3", 5, 0.05)
