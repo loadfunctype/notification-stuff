21dd2817d1e35e311058e693bbfc6516ef7fece9
loadfunctype <66913721+loadfunctype@users.noreply.github.com>
2025-03-21 01:16:07 -0300

    Create DocuHood.lua

---
+++ Documentaiton/DocuHood.lua
@@ -0,0 +1,2 @@
+local Lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/loadfunctype/notification-stuff/refs/heads/main/Modules/Hood.lua", true))()
+Lib:Notify("Title", "Bla bla bla", 10)
